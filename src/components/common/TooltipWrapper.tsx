5b5ba1ce6fb308dccecaa5bbc59b3c7dd8e5bf2a
CyberEzpertz <skyblaze24@gmail.com>
2024-12-21 09:31:23 +0800

    feat: create tooltip generic

---
+++ src/components/common/TooltipWrapper.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+
+interface TooltipWrapperProps {
+  children: ReactNode;
+  content: ReactNode;
+}
+
+export default function TooltipWrapper({
+  children,
+  content,
+}: TooltipWrapperProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent>{content}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
