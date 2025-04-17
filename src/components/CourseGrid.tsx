49d581a44158988ccb25dcd724036b2ffa414b33
CyberEzpertz <skyblaze24@gmail.com>
2025-04-18 04:38:47 +0800

    fix: fix delete course not working in course grid

--- src/components/CourseGrid.tsx
+++ src/components/CourseGrid.tsx
@@ -41,20 +41,21 @@ function CourseItem({ course, removeCourse }: CourseItemProps) {
       className={buttonVariants({
         variant: "outline",
         className: cn(
-          "cursor-grab justify-between",
+          "cursor-grab justify-between animate-out fade-out-0",
           isDragging && "opacity-30"
         ),
       })}
     >
       <span>{course.courseCode}</span>
-      <span
-        className="size-6 rounded-lg cursor-pointer flex items-center justify-center opacity-40 hover:opacity-100 group hover:bg-destructive hover:text-destructive-foreground transition-colors"
-        onMouseUp={() => {
+      <div
+        className="size-6 rounded-lg cursor-pointer flex items-center justify-center opacity-40 hover:opacity-100 group hover:bg-destructive hover:text-destructive-foreground transition-colors select-none"
+        onPointerDown={(e) => {
+          console.log("Removing course:", course.courseCode);
           removeCourse(course.courseCode);
         }}
       >
         <X className="size-3" strokeWidth={3} />
-      </span>
+      </div>
     </div>
   );
 }
@@ -325,19 +326,20 @@ export default function CourseGrid({}: CourseGridProps) {
               />
             ))}
             <DragOverlay dropAnimation={{ duration: 150, easing: "ease-out" }}>
-              {activeId && (
-                <div
-                  className={cn(
-                    buttonVariants({
-                      variant: "outline",
-                      className: "w-full",
-                    }),
-                    "justify-start"
-                  )}
-                >
-                  {activeId}
-                </div>
-              )}
+              {activeId &&
+                courses.find((course) => course.courseCode === activeId) && (
+                  <div
+                    className={cn(
+                      buttonVariants({
+                        variant: "outline",
+                        className: "w-full",
+                      }),
+                      "justify-start"
+                    )}
+                  >
+                    {activeId}
+                  </div>
+                )}
             </DragOverlay>
             <CreateGroupDialog
               onCreateGroup={handleCreateGroup}
